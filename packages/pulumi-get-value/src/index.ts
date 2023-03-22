ad4abffda5ef0d387eb674da7744c2dedb34e1c7
Alin Stefan Olaru <alin.stefan.olaru@LEGO.com>
2023-03-22 16:38:30 +0100

    feat: idk

--- packages/pulumi-get-value/src/index.ts
+++ packages/pulumi-get-value/src/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from '@pulumi/pulumi';
-
+console.log("Foo");
 export const getValue = async <T>(output: pulumi.Output<T>): Promise<T> => {
   return new Promise((resolve) => {
     output.apply((value) => {
